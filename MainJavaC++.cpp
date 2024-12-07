277d79d835ad12679a977ad1aac115d0a5428583
mokhtari250 <168132848+mokhtari250@users.noreply.github.com>
2024-12-06 21:18:43 -0600

    Create MainJavaC++.cpp
    This is a brand new description on C++ is created on Main Java

---
+++ MainJavaC++.cpp
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("SMASH THE LIKE button!");
+        System.out.println("DROP a COMMENT down below!");
+        System.out.println("SUBSCRIBE if you'd like to become a fellow bro!");
+
+        // This is a comment
+
+        /*
+         * This
+         * is
+         * a
+         * comment
+         */
+
+    }
+
+}
+
